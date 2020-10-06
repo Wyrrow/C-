debaffe6e92203f9668602e58a7dd2072a12d31e
Wyrrow <53649910+Wyrrow@users.noreply.github.com>
2020-10-06 18:18:24 +0300

    Add files via upload

---
+++ Lab1b_Pooo.cpp
@@ -0,0 +1,174 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+struct MyVector
+{
+    long* Elements;
+    int  Size;
+};
+
+void Create(MyVector& vector) 
+{
+    cout << "\nWhat size of the vector do you want?\nSize -  "; cin >> vector.Size;
+    vector.Elements = new long[vector.Size];
+    cout << "\nEnter " << vector.Size << " numbers into vector:\n";
+    for (int i = 0; i < vector.Size; i++)
+    {
+        cin >> vector.Elements[i];
+    }
+    
+}
+
+
+void Delete(MyVector& vector)
+{
+    delete[] vector.Elements;
+    vector.Elements = nullptr; 
+    vector.Size = NULL;
+}
+
+
+
+void Show(MyVector& vector)
+{
+    if (vector.Size == 0)
+    {
+        cout << "\nThis vector is empty!I'm so ssory" << endl;
+    }
+    else
+    {
+        cout << "\nVector have " << vector.Size << " elements" << endl;
+        for (int i = 0; i < vector.Size; i++)
+        {
+            cout << "[" << vector.Elements[i] << "]";
+        }
+    }
+}
+
+
+void Edit(MyVector& vector)
+{
+    int newSize;
+    cout << "\nEnter new size of vector:"; cin >> newSize;
+    if (vector.Size == newSize) {
+        cout << " Nothing has changed" << endl;
+        return;
+    }
+
+    MyVector tmp;
+    tmp.Size = newSize;
+
+    tmp.Elements = new long[newSize];
+
+
+    if (vector.Size > tmp.Size) {
+        for (int i = 0; i < tmp.Size; i++)
+        {
+            tmp.Elements[i] = vector.Elements[i];
+        }
+        cout << "\nThe new vector has" << (vector.Size - tmp.Size) << " elements less" << endl;
+    }
+    else {
+        for (int i = 0; i < vector.Size; i++)
+        {
+            tmp.Elements[i] = vector.Elements[i];
+        }
+        for (int i = vector.Size; i < tmp.Size; i++)
+        {
+            tmp.Elements[i] = 0;
+        }
+        cout << "\nThe new vector has" << (tmp.Size - vector.Size) << " more elements" << endl;
+    }
+    vector.Size = tmp.Size;
+    for (int i = 0; i < vector.Size; i++) {
+        vector.Elements[i] = tmp.Elements[i];
+    }
+
+    Show(vector);
+    Delete(tmp);
+
+}
+
+
+void SearchElement(MyVector const& vector)
+{
+    int item;
+    cout << "\nEnter the number of the desired item. \nEnter: "; cin >> item;
+    if (item > 0 && item < vector.Size)
+    {
+        cout << "\nYour element: " << vector.Elements[item] << endl;
+    }
+    else {
+        cout << "\nElement with number " << item << " doesn't exist" << endl;
+
+    }
+}
+
+
+
+void Sum(MyVector const& vector) {
+    long summ = 0;
+    for (int i = 0; i < vector.Size; i++)
+    {
+        if (vector.Elements[i] % 2 == 0)
+        {
+            summ = summ + vector.Elements[i];
+        }
+    }
+
+    cout << "\nThe sum of even elements of the vector is -> " << summ << endl;
+}
+
+
+void SearchMaxElement(MyVector& vector)
+{
+    int max = INT_MIN;
+    int pos;
+    for (int i = 0; i < vector.Size; i++)
+    {
+        if (max < vector.Elements[i])
+        {
+            max = vector.Elements[i];
+            pos = i;
+        }
+    }
+  cout << "\nMaximum vector element: Element [" << pos << "] =" << max << endl;
+}
+
+
+int main()
+{
+    MyVector vector1;
+    cout << "\nCreate vector!";
+    Create(vector1);
+
+    cout << "\nYour vector..";
+    Show(vector1);
+
+    cout << "\nEdit number of elements";
+    Edit(vector1);
+
+    cout << "\nSearch disired elemnt";
+    SearchElement(vector1);
+
+    cout << "\nSum of even elements";
+    Sum(vector1);
+
+    cout << "\nSearch max element";
+    SearchMaxElement(vector1);
+
+    cout << "\nDelete vector";
+    Delete(vector1); 
+    cout << "\nVector was deleted! Good buy! "; 
+    Show(vector1); 
+
+    return 0;
+
+
+
+}
+
+
---
+++ Lab2A_POO.cpp
@@ -0,0 +1,118 @@
+﻿// Lab2A_POO.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+class Major
+{
+   // friend ostream operator<<(ostream& os,  Major& Major);
+public: 
+    Major(int credits = 0 ,const string& teacher= " ",const string& subject =" ", const string& exam =" ");
+    Major(const Major& major);
+    ~Major(); 
+    Major& operator= ( Major& major); 
+    void Write();
+    void Show();
+       
+   
+private: 
+    string* m_pTeacher;
+    string* m_pSubject; 
+    string* m_pExam; 
+    int m_Credits; 
+};
+
+Major::Major(int credits, const string& teacher, const string& subject, const string& exam):
+    m_Credits(credits)
+{
+    m_pTeacher = new string(teacher); 
+    m_pSubject = new string(subject); 
+    m_pExam = new string(exam); 
+}
+
+Major::~Major()
+{
+    delete m_pTeacher; 
+    delete m_pSubject; 
+    delete m_pExam; 
+    m_Credits = NULL; 
+}
+
+
+Major::Major(const Major& major)
+{
+    m_pTeacher = new string(*(major.m_pTeacher)); 
+    m_pSubject = new string(*(major.m_pSubject)); 
+    m_pExam = new string(*(major.m_pExam)); 
+    m_Credits = major.m_Credits; 
+}
+
+Major& Major:: operator=(Major& major)
+{
+    cout << "\n--- Edit --- \n";
+
+    delete m_pTeacher;
+    m_pTeacher = new string(*(major.m_pTeacher));
+    cout << "\nWrite new techaer: "; cin >> *(major.m_pTeacher);
+    delete m_pSubject;
+    m_pSubject = new string(*(major.m_pSubject));
+    cout << "\nWrite new subject: "; cin >> *(major.m_pSubject);
+    delete m_pExam;
+    m_pExam = new string(*(major.m_pExam));
+    cout << "\nWrite new tipe of exam: "; cin >> *(major.m_pExam);
+
+    cout << "\nWrite new quantity of credits: "; cin >> major.m_Credits;
+
+    return *this;
+
+}
+
+ void Major :: Write()
+ {
+
+    cout << "\nWritee please --- \nTeacher: "; cin>> *m_pTeacher;
+
+    cout << "\nSubject:"; cin >> *m_pSubject; 
+
+    cout << "\nExam:"; cin >> *m_pExam;
+
+    cout << "\nCredits:"; cin >> m_Credits; 
+ 
+
+ }
+
+ void Major::Show()
+ {
+
+     cout << "\nYour major:";
+     cout << "\nTeacher: " << *m_pTeacher; 
+     cout << "\nSubject: " << *m_pSubject; 
+     cout << "\nExam: " << *m_pExam; 
+     cout << "\nCredits: " << m_Credits << endl; 
+ }
+
+
+int main()
+{
+    Major major1; 
+    Major major2; 
+    major1.Write(); 
+    major1.Show(); 
+    major2 = major1;
+    major1.Show(); 
+    return 0; 
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
